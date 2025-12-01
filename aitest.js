7c71ca68bfba8e0af60091b1e019871be4125804
Hayden Higginbotham <hayden@higginbotham.io>
2025-12-01 14:26:44 -0500

    Change prompt

--- aitest.js
+++ aitest.js
@@ -5,7 +5,7 @@ const aiClient = new OpenAI();
 (async () => {
   const response = await aiClient.responses.create({
     model: 'gpt-5-mini',
-    input: 'Write a short, pithy, grounded comment on philosophy or psychology in the style of a daily horoscope. Don\'t use any semicolons or em dashes.'
+    input: 'Write a short, pithy, vague prediction in the style of a fortune cookie. Don\'t use any semicolons or em dashes.'
   });
 
   console.log(response.output_text);
--- main.js
+++ main.js
@@ -37,7 +37,7 @@ var quote;
 async function newAIResponse() {
   const response = await aiClient.responses.create({
     model: 'gpt-5-mini',
-    input: 'Write a short, pithy, grounded comment on philosophy or psychology in the style of a daily horoscope. Don\'t use any semicolons or em dashes.'
+    input: 'Write a short, pithy, vague prediction in the style of a fortune cookie. Don\'t use any semicolons or em dashes.'
   });
 
   quote = response.output_text;
