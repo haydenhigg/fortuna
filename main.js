f18365c3aad0abac64ab428aba4538920ae6e1da
Hayden Higginbotham <hayden@higginbotham.io>
2025-12-01 13:02:10 -0500

    Remove sessions for now

--- main.js
+++ main.js
@@ -37,26 +37,34 @@ const prefix = '/fortuna'; // TODO: use fastify prefix with plugin
 fastify.register(require('@fastify/cookie'));
 fastify.register(require('./session.js'));
 
-const sessions = {};
+// const sessions = {};
 
 fastify.get(`${prefix}/daily`, function (request, reply) {
-  if (!Object.hasOwn(sessions, request.token)) {
-    let quote;
-    do {
-      quote = quotes[Math.floor(Math.random() * quotes.length)];
-    } while (quote.keywords.includes('bible') || quote.keywords.includes('faith'));
+  // if (!Object.hasOwn(sessions, request.token)) {
+  //   let quote;
+  //   do {
+  //     quote = quotes[Math.floor(Math.random() * quotes.length)];
+  //   } while (quote.keywords.includes('bible') || quote.keywords.includes('faith'));
 
-    sessions[request.token] = {
-      quote: `${quote.quote} - ${quote.author}`
-    };
-  }
+  //   sessions[request.token] = {
+  //     quote: `${quote.quote} - ${quote.author}`
+  //   };
+  // }
+
+  let quote;
+  do {
+    quote = quotes[Math.floor(Math.random() * quotes.length)];
+  } while (!quote.keywords.includes('life') || quote.keywords.includes('love') || quote.keywords.includes('bible') || quote.keywords.includes('faith'));
+
+  delete quote.keywords;
 
   const requestDetails = {
     timestamp: new Date(),
     token: request.token
   };
 
-  reply.send(Object.assign(requestDetails, sessions[request.token]));
+  // reply.send(Object.assign(requestDetails, sessions[request.token]));
+  reply.send(Object.assign(requestDetails, quote));
 });
 
 fastify.listen({ port: 8012 }, function (err, address) {
